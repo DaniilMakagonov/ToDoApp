6b6887b802046d46cb69209757eb8b152b34578b
Daniil Makagonov <daniil1makagonov@gmail.com>
2024-07-03 20:26:31 +0300

    Закончен дизайн карточки дела для списка

--- app/src/main/java/com/study/todoapp/data/Importance.kt
+++ app/src/main/java/com/study/todoapp/data/Importance.kt
@@ -1,7 +1,10 @@
 package com.study.todoapp.data
 
-enum class Importance {
-    Low,
-    Normal,
-    High
+import com.study.todoapp.R
+
+sealed class Importance(val name: String, val imageID: Int) {
+    data object Low : Importance("Низкая", R.drawable.low_importance)
+    data object Normal : Importance("Средняя", -1)
+    data object High : Importance("Высокая", R.drawable.high_importance)
+
 }
--- app/src/main/java/com/study/todoapp/ui/element/TodoItemCell.kt
+++ app/src/main/java/com/study/todoapp/ui/element/TodoItemCell.kt
@@ -1,24 +1,37 @@
 package com.study.todoapp.ui.element
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.study.todoapp.R
 import com.study.todoapp.data.Importance
 import com.study.todoapp.data.TodoItem
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun TodoItemCell(todoItem: TodoItem) {
@@ -26,27 +39,62 @@ fun TodoItemCell(todoItem: TodoItem) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(10.dp))
             .background(Color.White),
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Checkbox(
             checked = itemReady.value,
             onCheckedChange = {
                 itemReady.value = it
                 todoItem.isReady = it
-                              },
-            modifier = Modifier.padding(10.dp)
+            },
+            colors = if (todoItem.importance == Importance.High)
+                CheckboxDefaults.colors(uncheckedColor = Color.Red, checkedColor = Color.Green)
+            else CheckboxDefaults.colors(checkedColor = Color.Green)
         )
-        Text(
-            text = todoItem.text,
-            textDecoration = if (itemReady.value) TextDecoration.None else TextDecoration.LineThrough,
-            maxLines = 3,
-            overflow = TextOverflow.Ellipsis,
-            modifier = Modifier.padding(vertical = 2.dp)
-        )
+
+        if (!todoItem.isReady && todoItem.importance != Importance.Normal) {
+            Image(
+                painter = painterResource(id = todoItem.importance.imageID),
+                contentDescription = null,
+                modifier = Modifier.padding(end = 10.dp)
+            )
+        }
+
+        Column {
+            Text(
+                text = todoItem.text,
+                textDecoration = if (!itemReady.value) TextDecoration.None else TextDecoration.LineThrough,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+                    .fillMaxWidth(.85f)
+
+            )
+
+            if (!itemReady.value && todoItem.deadline != null) {
+                Text(
+                    text = "Сделать до: ${todoItem.deadline!!
+                        .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))}",
+                    color = Color.Gray,
+                    modifier = Modifier.padding(bottom = 3.dp)
+                )
+            }
+        }
+
+        IconButton(
+            onClick = { /*TODO*/ },
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.info),
+                contentDescription = null
+            )
+        }
     }
 }
-
 
 
 @Preview
@@ -58,9 +106,10 @@ fun TodoItemCellPreview() {
         text = "Очень важное дело, необходимость которого очень трудно переоценить. " +
                 "Именно поэтому каждое слово, написанное здесь, невероятно ценно. " +
                 "Спасибо, что мне хватило фантазии на это все.",
-        importance = Importance.Low,
+        importance = Importance.High,
         isReady = true,
-        creationDate = LocalDate.now()
+        creationDate = LocalDate.now(),
+        deadline = LocalDate.now().plusDays(7L)
     )
     TodoItemCell(todoItem = todoItem)
 
